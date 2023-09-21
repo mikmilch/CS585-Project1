39b5133ddac1727e2c2f8109e38943a471c4919d
mikmilch <mcmilch@wpi.edu>
2023-09-21 18:31:35 -0400

    adding taskB, need to test

--- Project1/src/main/java/TaskB.java
+++ Project1/src/main/java/TaskB.java
@@ -1,13 +1,64 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class TaskB {
 
+    public static class Map extends Mapper<Object, Text, Text, IntWritable>{
+
+        private Text outkey = new Text();
+        private final static IntWritable one = new IntWritable(1);
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] split = line.split(",");
+
+            for (String value : split) {
+                outkey.set(value[1]);
+                context.write(outkey, one);
+            }
+        }
+    }
+
+
+    public static class Reduce
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job task2 = Job.getInstance(conf, "job2");
+        Job job2 = Job.getInstance(conf, "Task B");
+
+        job2.setJarByClass(TaskB.class);
+        job2.setMapperClass(Map.class);
+        job2.setCombinerClass(TaskB.class);
+        job2.setReducerClass(Reduce.class);
+
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(IntWritable.class);
+
+        // maybe need to change args to args[1] and args[2]
+        FileInputFormat.addInputPath(job2, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+        System.exit(job2.waitForCompletion(true) ? 0 : 1);
     }
+}
 }
